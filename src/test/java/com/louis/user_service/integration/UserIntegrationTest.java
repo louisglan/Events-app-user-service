328d6837ddda855c98db2c338ed3a04bee308f01
Louis <louissam@hotmail.co.uk>
2025-10-05 10:47:05 +0100

    Added integration test for signup

---
+++ src/test/java/com/louis/user_service/integration/UserIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.louis.user_service.integration;
+
+import com.louis.user_service.configuration.SecurityConfig;
+import com.louis.user_service.utils.TestUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Import(SecurityConfig.class)
+public class UserIntegrationTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void testUserSignupSuccess() throws Exception {
+        mockMvc.perform(post("/api/users/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtils.getJsonString("/controller/validUser.json")))
+                .andExpect(status().isOk());
+    }
+}
