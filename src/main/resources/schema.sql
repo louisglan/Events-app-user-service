a74462f2cbe2ee7cf396fb01d1f393861f1c0ac4
Louis <louissam@hotmail.co.uk>
2025-10-06 20:41:40 +0100

    Refactored to use liquibase

--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-  email varchar(255) NOT NULL,
-  username varchar(255) NOT NULL,
-  date_of_Birth DATE NOT NULL,
-  role varchar(255) NOT NULL,
-  hashed_password varchar(255) NOT NULL
-);
